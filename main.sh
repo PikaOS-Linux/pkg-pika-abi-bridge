455f508e43b7ae073003088ac59282d6102fd9ad
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2024-07-21 13:44:29 +0300

    initial push

--- main.sh
+++ main.sh
@@ -2,10 +2,7 @@
 
 DEBIAN_FRONTEND=noninteractive
 
-# Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+cd ./pika-abi-bridge/
 
 # Get build deps
 apt-get build-dep ./ -y
